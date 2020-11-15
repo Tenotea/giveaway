bf051a1224ada9a289f678c94180028ee982d47a
Tenotea <drtenotea@gmail.com>
2020-11-15 23:31:13 +0100

    Allowed CORS acces from all origin

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const APP = require('./routes')
+const cors = require('cors')
 let PORT = process.env.PORT || 3000
+
+app.use(cors())
 APP(app)
 app.listen(PORT, () => {
   console.log('App running on port ' + PORT)
