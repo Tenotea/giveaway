38da133cea3170865a114790a8e312fbfff5cb15
Tenotea <drtenotea@gmail.com>
2020-11-15 22:20:36 +0100

    For real now, fixed db issue

--- Participants.js
+++ Participants.js
@@ -1,4 +1,4 @@
 const mongoDB = require('mongodb')
 require('dotenv').config()
 
-module.exports = async () => (await mongoDB.MongoClient.connect(process.env.DATABASE_URI, {useUnifiedTopology: true, useNewUrlParser: true})).db('giveway')
+module.exports = async () => (await mongoDB.MongoClient.connect(process.env.DATABASE_URI, {useUnifiedTopology: true, useNewUrlParser: true})).db('giveaway')
